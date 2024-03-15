8bd4658223f238c0e500c3e65d53694ba339cb64
swathi <swathibalasubramanyam@gmail.com>
2024-03-14 17:39:47 -0700

    Base home page

--- marketplace/marketplace/urls.py
+++ marketplace/marketplace/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from core.views import index
+from core.views import index, contact
 
 urlpatterns = [
-    path('core/', include("core.urls")),
+    path('contact/', contact, name="contact"),
     path('admin/', admin.site.urls),
+    path('', index),
 ]
